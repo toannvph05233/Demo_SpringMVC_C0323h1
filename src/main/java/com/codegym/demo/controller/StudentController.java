f5186d05a6939646c26b28a1a9fdc363b590e729
toannvph05233 <johntoan98@gmail.com>
2023-07-07 14:38:08 +0700

    up bean

--- src/main/java/com/codegym/demo/controller/StudentController.java
+++ src/main/java/com/codegym/demo/controller/StudentController.java
@@ -2,10 +2,12 @@ package com.codegym.demo.controller;
 
 import com.codegym.demo.model.Student;
 import com.codegym.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class StudentController {
-    StudentService studentService = new StudentService();
+    @Autowired
+    StudentService studentService;
 
     @GetMapping("/students")
     public String showStudent(Model model) {
@@ -22,14 +25,14 @@ public class StudentController {
     }
 
     @GetMapping("/editStudent")
-    public String showEditStudent(int id, Model model) {
+    public String showEditStudent(@RequestParam int id, Model model) {
         model.addAttribute("student", studentService.findById(id));
         return "editStudent";
     }
 
     @PostMapping("/editStudent")
-    public String showEditStudent(int id, int age, String name, String img) {
-        studentService.editStudents(id, new Student(id, age, name, img));
+    public String showEditStudent(@ModelAttribute Student student) {
+        studentService.editStudents(student.getId(),student);
         return "redirect:/students";
     }
 
